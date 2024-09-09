32e8ada199c6c6ec9ca3da098ea0cbedb2e8b918
Chris Koorzen <fckoorzen@gmail.com>
2024-09-09 22:43:05 +0200

    Add 'concept', fix function input parameter, fix function method call.

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -9,11 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     document.getElementById("solveRoom2").addEventListener("click", () => {
-        const jsConcepts = new Set(['closure', 'scope', 'hoisting']);
-        // 🪲 Bug: What's mssing from JS concepts?
+        const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'async']);
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
-        // 🪲 Bug: Incorrect function call
-        const commonConcepts = findIntersection(jsConcepts, jsConcepts);
+        const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
@@ -38,8 +36,7 @@ function findMostRecentBook(books) {
 }
 
 function findIntersection(setA, setB) {
-    // 🪲 Bug: Incorrect logic
-    const intersection = new Set([...setA]);
+    const intersection = setA.intersection(setB);
     return intersection;
 }
 
